2fe53b97e41a54eaa4ca480962c0f1d97c87868c
farah seddik <farahseddik500@gmail.com>
2023-05-27 17:42:50 +0100

    ..

--- frontend/src/pages/preview.tsx
+++ frontend/src/pages/preview.tsx
@@ -3,5 +3,17 @@ import PreviewHeader from "../components/PreviewHeader.tsx";
 export default function Preview() {
     return(
         <PreviewHeader/>
+        <div style={{ height: '1000px', display: 'flex', alignItems: 'center' }}>
+      <div style={{ flex: 1, textAlign: 'left' }}>
+        <div style={{ padding: '20px' }}>
+          <h1>Text Box</h1>
+          <p>This is some text inside the box.</p>
+          <button>Click me</button>
+        </div>
+      </div>
+      <div style={{ flex: 1 }}>
+        <img src="your-image-url.jpg" alt="Your Image" style={{ width: '100%', height: '100%' }} />
+      </div>
+    </div>
     )
 }
