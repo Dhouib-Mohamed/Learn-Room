cb3878d026e3a95c1e26fe0ea884dd01666f0c8d
farah seddik <farahseddik500@gmail.com>
2023-05-27 19:26:11 +0100

    landing page front

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,25 @@
-import {Box, Heading} from '@chakra-ui/react';
+import { Box, Button, Flex, HStack, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.png";
 
 const Header = () => {
     return (
-        <Box bg="blue.500" w="100%" p={4} color="white">
-            <Heading>Google Classroom Clone</Heading>
-        </Box>
+        <Flex
+        as="header"
+        align="center"
+        justify="space-between"
+        py="4"
+        px="6"
+        bg="white"
+        borderBottom="0.75px solid #828282"
+    >
+        <HStack spacing={"2px"}>
+            <img src={logo} alt="Logo" style={{ height: "42px", marginRight: "10px", marginLeft: "10px" }} />
+            <Text fontSize="2xl" fontWeight="bolder" color="gray.700">
+                LearnRoom
+            </Text>
+        </HStack>
+        
+    </Flex>
     );
 };
 
