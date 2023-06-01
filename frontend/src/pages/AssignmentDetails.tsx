595b2c48816b46ca3074c766d161298cbc4727d8
nadamankai <nada.mankai@insat.ucar.tn>
2023-06-01 18:12:13 +0100

    fixing the task details and AssignmentDetails pages

--- frontend/src/pages/AssignmentDetails.tsx
+++ frontend/src/pages/AssignmentDetails.tsx
@@ -1,8 +1,10 @@
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import { Button, Divider } from "@chakra-ui/react";
+import {Button, Divider, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, useDisclosure} from "@chakra-ui/react";
 import {classrooms} from "../data/classrooms.js";
 import {useParams} from "react-router-dom";
+import {getItem} from "../../utils/localStorage";
+import {AiOutlineMore} from "react-icons/ai";
 
 
 function AssignmentDetails() {
@@ -10,29 +12,74 @@ function AssignmentDetails() {
     console.log(id)
     console.log(classrooms[0])
     const assignment=classrooms[0].assignments[0];
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const deleteClassroom = async () => {
+        // const response=  await remove ("classroom/"+id)
+        // setItem("user",response)
+        // history.push('/home');
+    }
+
+    const editClassroom = async (data) => {
+        // const result = await patch("classroom/" + id, data)
+        // console.log("res:", result)
+        // setItem("user", result)
+        // onClose();
+        // setUpdate(!update)
+    }
+
+    const handleSubmit = (values) => {
+
+        editClassroom(values)
+
+    };
+
+
     return (
         <>
         <div style={{minHeight: "89vh",}}>
         <Header />
-        <br />
-            <br />
-            <div style={{
-                display: 'flex', alignItems: 'center', justifyContent: "center"
-            }}>
-                <div style={{ width: "1000px", }}>
-                    <h3 style={{ fontWeight: '600', fontSize: "24px" }}>{assignment.title}</h3>
+
+            <Flex style={{
+                flexDirection:"column",  justifyContent: "center" , margin:"20px 100px" }}>
+
+                  <Flex flexDirection={"row"}>
+                      <div style={{width:"99%"}}>
+                      <h3 style={{ fontWeight: '600', fontSize: "24px" }}>{assignment.title}</h3>
                     <p style={{ fontSize: '15px', fontWeight: 'lighter' }}>
                         {assignment.teacher}  &#9679; {assignment.date}
                     </p>
                     <div style={{ height: '10px' }}></div>
                     <p style={{ fontSize: '16px', fontWeight: 'normal' }}>
                         {assignment.points} points
-                    </p>
+                    </p> </div>
+
+                    {getItem("user").user ?  <Menu flip={true} direction={"rtl"} >
+                        <MenuButton
+                            fontSize={"25px"}
+                            style={{marginTop:2 }}
+                            as={IconButton}
+                            aria-label='Options'
+                            icon={<AiOutlineMore/>}
+                            variant='outline'
+                            border={false}
+                        />
+                        <MenuList  style={{fontSize:"15px"}}>
+                            <MenuItem onClick={()=> {onOpen()
+
+                            }} >Edit Classroom</MenuItem>
+
+                            <MenuItem onClick={deleteClassroom} >Delete Classroom</MenuItem>
+
+                        </MenuList>
+
+                    </Menu> : null }
+
+
+                  </Flex>
                     <Divider width="100%" my={4} borderColor={"#A6A6A6"} borderWidth="0.75px" />
-                    <div style={{ width: "1000px", display: 'flex', alignItems: 'center', justifyContent: "center" }}>
-                        <div style={{ width: "900px" }}>
+
+                    <div style={{ width: "100%" }}>
                             <pre >{assignment.description}</pre>
-                        </div>
                     </div>
                     <br />
                     <div style={{ display: 'flex', alignItems: 'center', justifyContent: "center"}}>
@@ -42,8 +89,9 @@ function AssignmentDetails() {
                     borderColor="grey" rounded="full"  type="submit"  my="4"  onClick={() => {
                     }}>Submit homework</Button>
                     </div>
-                </div>
-            </div></div>
+
+                </Flex>
+            </div>
         <Footer/>
         </>
     );
