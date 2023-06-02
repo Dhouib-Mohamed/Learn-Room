95af1c8439b3c9ebb08f420aa456d1f4d9fd5bbe
farah seddik <farahseddik500@gmail.com>
2023-06-02 23:21:50 +0100

    minor fix

--- frontend/src/components/TaskButton.tsx
+++ frontend/src/components/TaskButton.tsx
@@ -12,14 +12,14 @@ function TaskButton({completed, handleChange}) {
     return (
         <div style={{display: "flex", flexDirection: "row",width:'360px' }}>
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: "center", flexDirection: 'column' }}>
-            {completed && <div><p style={{ fontSize: '16px', fontWeight: 'normal' }}>
+            {!completed && <div><p style={{ fontSize: '16px', fontWeight: 'normal' }}>
                 You finished this task?
             </p></div>}
             <div style={{display: "flex", flexDirection: "row" }}>
                 <Button colorScheme="custom"
                 color="white"
-                bgColor={completed ? "#66B0F0" : "#FF8076"} rounded="full" width="250px" size="md" onClick={handleChange} marginTop={!completed?'70px':'46px'} >
-                {completed ? "Mark as Completed" : "Well done"}
+                bgColor={completed ?   "#66B0F0":"#FF8076"} rounded="full" width="250px" size="md" onClick={handleChange} marginTop={!completed?'70px':'46px'} >
+                {!completed ? "Mark as Completed" : "Well done"}
             </Button>
             </div><br />
             <br />
@@ -27,7 +27,7 @@ function TaskButton({completed, handleChange}) {
             <br />
             <br />
         </div>
-        {!completed && <img src={gradcap} style={{height:'100px'}} alt="Image" />}
+        {completed && <img src={gradcap} style={{height:'100px'}} alt="Image" />}
 
         </div>
     );
